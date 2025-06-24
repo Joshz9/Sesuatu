956b5ef1f36a504420648d5fe4a20d8b9bfaaceb
Joshz9 <joshpunk7855@gmail.com>
2025-06-24 21:47:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+document.getElementById('klikGue').addEventListener('click', function() {
+  const output = document.getElementById('output');
+  output.textContent = 'Lo baru aja ngeklik tombol, keren bro! 🚀';
+});
